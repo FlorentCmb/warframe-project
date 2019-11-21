a224a03599fbe41dbde6af646406dc56891b7f68
FlorentCmb <florent.cmb77@gmail.com>
2019-11-21 17:44:23 +0100

    Add react-router methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Import librairies
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 // Import styles
 import './App.css'
@@ -8,6 +9,23 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        {/* Header */}
+        <Switch>
+
+          <Route>
+            {/* Home */}
+          </Route>
+          <Route>
+            {/* Warframe */}
+          </Route>
+          <Route>
+            {/* Weapons */}
+          </Route>
+          <Route>
+            {/* Mods */}
+          </Route>
+
+        </Switch>
       </div>
     )
   }
