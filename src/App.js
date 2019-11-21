14d383da5f3aebd03992ef86e014faa5b716310c
Nelo <kevinpplle@gmail.com>
2019-11-21 17:47:46 +0100

    added section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 // Import librairies
 import React from 'react'
 
-import React from "react";
-
 
 // Import styles
 import './App.css'
 
 export default class App extends React.Component {
+
   render() {
+
     return (
       <div className="App">
+        <div className="section1">
+          <h1>JAJ</h1>
+        </div>
+        <div className="section2">
+          JOJ
+        </div>
+        <div className="section3">
+          JEJ
+        </div>
       </div>
     )
   }
