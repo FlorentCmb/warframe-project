d465c36cdd64c221e7f5b47ac786e6c1465f860d
FlorentCmb <florent.cmb77@gmail.com>
2019-11-22 08:50:33 +0100

    Add style to the header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 
 // Import styles
 import './App.css'
+import Header from './components/Header'
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 // Import librairies
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
+// Import styles
+import './Header.css'
 
 export default class Header extends React.Component {
 
     render() {
         return(
             <div className="Header">
-                <Link className="Header-Link" to="/">Home</Link>
+                <a className="Header-Link" href="/"><span className="Header-Text">Home</span></a>
+                <a className="Header-Link" href="/warframes"><span className="Header-Text">Warframes</span></a>
+                <a className="Header-Link" href="/weapons"><span className="Header-Text">Weapons</span></a>
+                <a className="Header-Link" href="/mods"><span className="Header-Text">Mods</span></a>
             </div>
         )
     }
