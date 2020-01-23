063084aa56b213d213481edb5eddb704b5ba0f6f
FlorentCmb <florent.cmb77@gmail.com>
2020-01-23 12:05:13 +0100

    Modify header's default theme

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -9,30 +9,28 @@ export default class Home extends React.Component {
             <div className="Home-Page">
 
                 <div className="Home-Section Home-Warframe">
-                    <h1>HELLO SA MERE</h1>
+                    HELLO SA MERE
                 </div>
 
-                <div className="Home-Banner Home-Background-1">
-                </div>
+                <div className="Home-Banner Home-Background-1" />
 
 
                 <div className="Home-Section Home-Site">
-                    <h2>Website Description</h2>
+                    Website Description
                 </div>
 
-                <div className="Home-Banner Home-Background-2">
-                </div>
+                <div className="Home-Banner Home-Background-2" />
 
 
 
                 <div className="Home-Section Home-Team">
                     <div className="Profil-Florent">
-                        <h1>Asod - Florent Combeau</h1>
+                        Asod - Florent Combeau
                         <img src="../pictures/asod.png" />
                     </div>
 
                     <div className="Profil-Kevin">
-                        <h1>Nelo - Kevin Pompelle</h1>
+                        Nelo - Kevin Pompelle
                         <img src="../pictures/nelo.png" />
 
                     </div>
