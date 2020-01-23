763d5e2630dca7b2d907f7bf0121925377909ae7
Nelo <kevinpplle@gmail.com>
2020-01-23 11:53:03 +0100

    rework website

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -7,19 +7,35 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="Home-Page">
+
                 <div className="Home-Section Home-Warframe">
-                    <h1>Warframe</h1>
-                    <p className="Home-Paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
-                </div>
-                
-                <div className="Home-Section Home-Site">
-                    <h1>About nous</h1>
-                    <p className="Home-Paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+                    <h1>HELLO SA MERE</h1>
                 </div>
 
+                <div className="Home-Banner Home-Background-1">
+                </div>
+
+
+                <div className="Home-Section Home-Site">
+                    <h2>Website Description</h2>
+                </div>
+
+                <div className="Home-Banner Home-Background-2">
+                </div>
+
+
+
                 <div className="Home-Section Home-Team">
-                    <h1>Our partenaires</h1>
-                    <p className="Home-Paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+                    <div className="Profil-Florent">
+                        <h1>Asod - Florent Combeau</h1>
+                        <img src="../pictures/asod.png" />
+                    </div>
+
+                    <div className="Profil-Kevin">
+                        <h1>Nelo - Kevin Pompelle</h1>
+                        <img src="../pictures/nelo.png" />
+
+                    </div>
                 </div>
             </div>
         )
